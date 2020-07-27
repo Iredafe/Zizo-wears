011f823b6500e1c1621687945229a081dc321e3a
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-07-27 16:32:56 -0700

    create directory component & menu items

--- src/App.js
+++ src/App.js
@@ -1,8 +1,5 @@
 import React, { Component } from 'react';
-import {HomePage} from "./homepage.component"
-import "./homepage.styles.scss"
-
-
+import {HomePage} from "./components/pages/homepage/homepage.component"
 
 class App extends Component {
   render() {
---
+++ src/components/pages/homepage/homepage.component.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+import "./homepage.styles.scss"
+import Directory from "../../directory/directory.component";
+
+export const HomePage = ()=>(
+    <div className="homepage">
+<Directory />
+   </div>
+)
