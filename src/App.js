70706e9f77ea94479afc9be198601704fdbec068
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-07-26 22:16:57 -0700

    create homepage component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
-import './App.css';
+import {HomePage} from "./homepage.component"
+import "./homepage.styles.scss"
+
+
 
 class App extends Component {
   render() {
-    return <div className='App'>Hello World</div>;
+    return <div className='App'>
+      <HomePage />
+    </div>;
   }
 }
 
