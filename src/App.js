4bf22c4a75c6480dbb1ad5b372be977ba861c361
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-07-27 18:05:10 -0700

    break home page into smaller components and style with sass

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {HomePage} from "./components/pages/homepage/homepage.component"
+import "./App.css"
 
 class App extends Component {
   render() {
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -26,14 +26,14 @@ sections: [{
       linkUrl: 'shop/sneakers'
     },
     {
-      title: 'womens',
+      title: "women's",
       imageUrl: 'https://i.ibb.co/GCCdy8t/womens.png',
       size: 'large',
       id: 4,
       linkUrl: 'shop/womens'
     },
     {
-      title: 'mens',
+      title: "men's",
       imageUrl: 'https://i.ibb.co/R70vBrQ/men.png',
       size: 'large',
       id: 5,
