4a6f497ec80d526cfeafa80cc01d122d01e6abcd
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-08-01 14:13:20 -0700

    collection-preview component

--- src/App.js
+++ src/App.js
@@ -2,20 +2,18 @@ import React from 'react';
 import {HomePage} from "./components/pages/homepage/homepage.component"
 import "./App.css"
 import { Switch, Route } from 'react-router-dom';
-
-const HatsPage=()=>(<div>
-  <h1>HATSPAGE</h1>
-  </div>)
+import ShopPage from "./components/pages/shop/shop.component.jsx"
 
 
 function App() {
-    return <div className='App'>
-    <Switch>
-      <Route exact path="/" component={HomePage}></Route>
-    <Route exact path="/hats" component={HatsPage}></Route>
-    </Switch>
-      <HomePage />
-    </div>;
+    return(
+    <div>
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+      <Route path="/shop" component={ShopPage} />
+      </Switch>
+      </div>
+      ) 
   }
 
 export default App;
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -11,7 +11,7 @@ sections: [{
       title: 'hats',
       imageUrl: 'https://i.ibb.co/cvpntL1/hats.png',
       id: 1,
-      linkUrl: '/hats'
+      linkUrl: 'shop/hats'
     },
     {
       title: 'jackets',
