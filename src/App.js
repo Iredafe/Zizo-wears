bb6c3b2e1fbc185a30511b5b6dcdde0e731ea705
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-08-02 18:22:17 -0700

    build header component

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import {HomePage} from "./components/pages/homepage/homepage.component"
 import "./App.css"
 import { Switch, Route } from 'react-router-dom';
 import ShopPage from "./components/pages/shop/shop.component.jsx"
+import Header from "./components/header/header.component"
 
 
 function App() {
     return(
     <div>
+    <Header />
       <Switch>
         <Route exact path="/" component={HomePage} />
       <Route path="/shop" component={ShopPage} />
