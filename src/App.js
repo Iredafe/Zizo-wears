1fe7aad9a86d8e727a73399009b2bbdf1d2cbdc2
Iredafe Owolabi <iredafeowolabi@gmail.com>
2020-08-05 23:42:08 -0700

    set up persistence to firestore

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import { Switch, Route } from 'react-router-dom';
 import ShopPage from "./components/pages/shop/shop.component.jsx"
 import Header from "./components/header/header.component"
 import SignInAndSignUpPage from './components/pages/sign-in-and-sign-up/sign-in-and-sign-up.component';
-import {auth} from "../src/firebase/firebase.utils"
+import {auth, createUserProfileDocument} from "../src/firebase/firebase.utils"
 
 class App extends React.Component {
   constructor(){
@@ -19,9 +19,8 @@ class App extends React.Component {
   unsubscribeFromAuth=null;
 
   componentDidMount(){
-   this.unsubscribeFromAuth= auth.onAuthStateChanged(user=>{
-      this.setState({currentUser : user});
-      console.log(user)
+   this.unsubscribeFromAuth= auth.onAuthStateChanged(async user=>{
+createUserProfileDocument(user);
     })
   }
 
--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -2,7 +2,6 @@ import firebase from "firebase/app"
 import "firebase/firestore"
 import "firebase/auth"
 
-
 const config= {
     apiKey: "AIzaSyD-ZV7BKIcNyxuheiH1FLqNhm63kVwE8vA",
     authDomain: "zizo-wears-db.firebaseapp.com",
@@ -14,6 +13,30 @@ const config= {
     measurementId: "G-9BNX0N4BPS"
   };
 
+export const createUserProfileDocument = async(userAuth, additionalData)=>{
+  if (!userAuth) return;
+  const userRef = firestore.doc(`users/${userAuth.uid}`)
+  const snapShot = await userRef.get();
+
+  if(!snapShot.exists){
+    const{displayName, email} =userAuth;
+    const createdAt = new Date();
+
+try{
+  await userRef.set({
+    displayName,
+    email,
+    createdAt,
+    ...additionalData
+  })
+
+}catch(error){
+console.log('error creating user', error.message)
+
+}
+  } 
+return userRef;
+}
 
   firebase.initializeApp(config);
 
